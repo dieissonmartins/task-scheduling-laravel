fead590e4e3692ee82cddfe1bf0774347cee4c97
Dieisson Martins <dieisson.martins.santos@gmail.com>
2023-02-02 11:45:23 -0300

    estudos de padrao de comentarios

--- app/Http/Controllers/SendMailController.php
+++ app/Http/Controllers/SendMailController.php
@@ -3,13 +3,41 @@
 namespace App\Http\Controllers;
 
 use App\Events\SendMail;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SendMailController extends Controller
 {
+    /**
+     * @param $email_origin
+     * @return string
+     */
     public function show($email_origin): string
     {
         SendMail::dispatch($email_origin);
 
         return "email added to queue;";
+    }
+
+    # methods nao functional someones para testes
+
+    /**
+     * @param Request $request
+     * @return array
+     */
+    public function store(Request $request): array
+    {
+        $data = $request->all();
+
+        DB::transaction(function () use ($data, &$user) {
+
+            $user = User::create($data);
+
+        });
+
+        return [
+            'data' => $user
+        ];
     }
 }
